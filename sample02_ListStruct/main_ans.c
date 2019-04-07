dadc2cd8db8b3a19e68f423ce285139c54b9a74d
mayoneka <myonekawa@myonekawa.fareast.corp.microsoft.com>
2019-04-07 18:35:36 +0900

    add main_ans.c

---
+++ sample02_ListStruct/main_ans.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  /* 構造体 result を定義 */
+  typedef struct _result
+  {
+    short int id;
+    char name[16];
+    short int score;
+    struct _result *next;
+  } result;
+
+  int i;             /* ループカウンタ */
+  result *p, *start; /* 構造体のポインタ */
+  result *temp;      /* 諸々で使う一時ポインタ */
+
+  /* 元データを配列で準備 */
+  char _name[5][16] = {"Taro", "Hanako", "Sayaka", "Miki", "Hiroshi"};
+  short int _score[5] = {50, 60, 70, 80, 30};
+
+  for (i = 0; i < 5; i++)
+  {
+    if (i == 0)
+    {
+      /* 先頭の構造体を生成 */
+      p = malloc(sizeof(result));
+      /* メモリ確保した領域を0クリア */
+      memset(p, 0, sizeof(p));
+      start = p; /* 先頭を保存 */
+    }
+    else
+    {
+      p->next = malloc(sizeof(result));
+      memset(p->next, 0, sizeof(p->next));
+      p = p->next;
+    }
+    /* IDはカウンタ変数から生成 */
+    p->id = (short int)i + 1;
+    /* 2項目を代入 */
+    strcpy(p->name, _name[i]);
+    p->score = _score[i];
+    p->next = NULL;
+  }
+
+  /* 構造体のリンクをたどって先頭からメンバを表示 */
+  printf("ID\tName\tScore\n");
+  p = start; /* ポインタを先頭に */
+  while (p != NULL)
+  {
+    printf("%04d\t%s\t%d\n", p->id, p->name, p->score);
+    p = p->next;
+  }
+
+  /* 課題 1. 先頭から 3 番目までの人を削除してから表示する */
+  printf("------------task 1-------------- \n");
+  p = start; // ポインタを先頭に
+  for (i = 0; i < 3; i++)
+  {
+    temp = p->next;
+    printf("delete %s\n", p->name);
+    free(p);
+    start = p = temp;
+  }
+
+  /* 構造体のリンクをたどって先頭からメンバを表示 */
+  printf("ID\tName\tScore\n");
+  p = start; /* ポインタを先頭に */
+  while (p != NULL)
+  {
+    printf("%04d\t%s\t%d\n", p->id, p->name, p->score);
+    p = p->next;
+  }
+
+  /* 課題 2. 2人追加する */
+  printf("------------task 2-------------- \n");
+  char _name2[2][16] = {"Mike", "Jhon"};
+  short int _score2[2] = {90, 90};
+
+  // 末尾まで移動する
+  p = start;
+  if (p != NULL)
+  {
+    while (p->next != NULL)
+    {
+      p = p->next;
+    }
+  }
+  for (i = 0; i < 2; i++)
+  {
+    p->next = malloc(sizeof(result));
+    memset(p->next, 0, sizeof(p->next));
+    p = p->next;
+
+    p->id = (short int)i + 6;
+    strcpy(p->name, _name2[i]);
+    p->score = _score2[i];
+    p->next = NULL;
+  }
+
+  /* 構造体のリンクをたどって先頭からメンバを表示 */
+  printf("ID\tName\tScore\n");
+  p = start; /* ポインタを先頭に */
+  while (p != NULL)
+  {
+    printf("%04d\t%s\t%d\n", p->id, p->name, p->score);
+    p = p->next;
+  }
+
+  /* メモリクリア */
+  while (p != NULL)
+  {
+    temp = p->next;
+    free(p);
+    p = temp;
+  }
+  return (0);
+}
